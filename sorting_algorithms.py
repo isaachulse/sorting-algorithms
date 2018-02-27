2cb41a0fa6365dd7a8ca94f3bc061ad5a69097ac
Isaac Hulse <isaac.hulse@hotmail.com>
2018-02-27 15:44:44 +0000

    completed files

---
+++ sorting_algorithms.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+import sys
+
+################################################################################
+##                                                                            ##
+##  PYTHON  3 IMPLEMENTATION OF BUBBLE SORT, QUICK SORT AND SELECTION SORT,   ##
+##  USING A CONVERT FUNCTION TO STREAMLINE MIXED TYPE LISTS INTO FLOATS &     ##
+##  INTS. MADE BY ISAAC HULSE AS PART OF EBUYER DEVELOPER PROJECT :)          ##
+##                                                                            ##
+################################################################################
+
+def convert_list(mixed_list):
+    list_len = len(mixed_list)
+    unsorted_list = []
+    for i in range(list_len):
+        if isinstance(mixed_list[i], int) or isinstance(mixed_list[i], float):
+            unsorted_list.append(mixed_list[i])
+        elif isinstance(mixed_list[i], str):
+            try:
+                unsorted_list.append(float(mixed_list[i]))
+            except ValueError:
+                unsorted_list.append(None)
+        else:
+            unsorted_list.append(None)
+    unsorted_list = [x for x in unsorted_list if x != None]
+    return unsorted_list
+
+def bubble_sort(mixed_list):
+    unsorted_list = convert_list(mixed_list)
+    list_len = len(unsorted_list)
+    if list_len <= 1:
+        sorted_list = unsorted_list
+        return sorted_list
+    else:
+        for i in range(list_len):
+            for j in range(list_len - 1):
+                if unsorted_list[j] > unsorted_list[j + 1]:
+                    tmp = unsorted_list[j]
+                    unsorted_list[j] = unsorted_list[j + 1]
+                    unsorted_list[j + 1] = tmp
+        sorted_list = unsorted_list
+    return sorted_list
+
+
+def quick_sort(mixed_list):
+    unsorted_list = convert_list(mixed_list)
+    if len(unsorted_list) < 2:
+        sorted_list = unsorted_list
+        return sorted_list
+    else:
+        pivot_point = unsorted_list[0]
+        larger_list = [num for num in unsorted_list[1:] if num > pivot_point]
+        smaller_list = [num for num in unsorted_list[1:] if num <= pivot_point]
+        return quick_sort(smaller_list)+[pivot_point]+quick_sort(larger_list)
+
+def selection_sort(mixed_list):
+    unsorted_list = convert_list(mixed_list)
+    list_length = len(unsorted_list)
+    for val in range(list_length):
+        leastVal = val
+        for compareVal in range(val+1, list_length):
+            if unsorted_list[compareVal] < unsorted_list[leastVal]:
+                leastVal = compareVal
+        tmp = unsorted_list[leastVal]
+        unsorted_list[leastVal] = unsorted_list[val]
+        unsorted_list[val] = tmp
+    sorted_list = unsorted_list
+    return sorted_list
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        mixed_list = []
+        # Slice off sys.argv[0] (program filename, sorting_algorithms.py)
+        for element in sys.argv[2:]:
+            mixed_list.append(element)
+    else:
+        sys.exit(1)
+
+    if sys.argv[1] == 'bubble':
+        print(bubble_sort(mixed_list))
+    elif sys.argv[1] == 'quick':
+        print(quick_sort(mixed_list))
+    elif sys.argv[1] == 'selection':
+        print(selection_sort(mixed_list))
+    else:
+        print("Not Valid Sorting Algorithm")
+        sys.exit(1)
+
+
---
+++ test_sorting_algorithms.py
@@ -0,0 +1,53 @@
+from sorting_algorithms import *
+import unittest
+
+################################################################################
+##                                                                            ##
+##  UNIT TESTING OF PYTHON IMPLEMENTED SEARCH ALGORITHMS FOR EBUYER PROJECT   ##
+##                                                                            ##
+################################################################################
+
+class main(unittest.TestCase):
+
+
+    def test_pass_int_through(self):
+        self.assertEqual(convert_list([-36, -1, 0, 1, 35]), [-36, -1, 0, 1, 35])
+
+    def test_pass_float_through(self):
+        self.assertEqual(convert_list([234.64, 235.62, 2.2346, 0.1234]), [234.64, 235.62, 2.2346, 0.1234])
+
+    def test_ignore_strings_with_characters(self):
+            self.assertEqual(convert_list(['foo', 'bar', 'foo7bar']), [])
+
+    def test_convert_num_strings_into_nums(self):
+            self.assertEqual(convert_list(['235.34', '0.3521', '12', '48']), [235.34, 0.3521, 12.0, 48.0])
+
+    def test_convert_mixed_list(self):
+            self.assertEqual(convert_list(['235.34', 'marsh', 'snowflake' '0.3521', '12', '48', 12, 13, 56783, 0.2364, 0.43, 9863.0]),[235.34, 12.0, 48.0, 12, 13, 56783, 0.2364, 0.43, 9863.0])
+
+    def test_bubble_sort_empty(self):
+        self.assertEqual(bubble_sort([]), [])
+
+    def test_selection_sort_empty(self):
+        self.assertEqual(selection_sort([]), [])
+
+    def test_quick_sort_empty(self):
+        self.assertEqual(quick_sort([]), [])
+
+    def test_bubble_sort_mixed(self):
+        mixed_list = ['13556', 'hello', 12.532, 2678, 0, '0', '56bar', 'f00', '325.542', 0.123]
+        mixed_list_sorted = [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0]
+        self.assertEqual(bubble_sort(mixed_list), [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0])
+
+    def test_quick_sort_mixed(self):
+        mixed_list = ['13556', 'hello', 12.532, 2678, 0, '0', '56bar', 'f00', '325.542', 0.123]
+        mixed_list_sorted = [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0]
+        self.assertEqual(quick_sort(mixed_list), [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0])
+
+    def test_selection_sort_mixed(self):
+        mixed_list = ['13556', 'hello', 12.532, 2678, 0, '0', '56bar', 'f00', '325.542', 0.123]
+        mixed_list_sorted = [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0]
+        self.assertEqual(selection_sort(mixed_list), [0, 0.0, 0.123, 12.532, 325.542, 2678, 13556.0])
+
+if __name__ == '__main__':
+    unittest.main()
